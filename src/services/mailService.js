d0401807ec19c43907acb83473ad869acf5ea65e
Nguyen Hong Quoc Truong <ongdeo4369@gmail.com>
2026-08-01 23:51:11 +0700

    fix email

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -2,11 +2,16 @@ const nodemailer = require("nodemailer");
 const QRCode = require("qrcode");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 exports.sendOTPEmail = async (email, otp) => {
@@ -26,7 +31,7 @@ exports.sendOTPEmail = async (email, otp) => {
 };
 
 exports.sendBookingSuccessEmail = async (email, bookingInfo) => {
-  const bookingUrl = `http://localhost:5173/khachhang/booking/${bookingInfo.MaDP}`;
+  const bookingUrl = `${process.env.FRONTEND_URL}/khachhang/booking/${bookingInfo.MaDP}`;
 
   const qrBuffer = await QRCode.toBuffer(bookingUrl, {
     width: 220,
